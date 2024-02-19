58ca5b97ad27a00bffdc75ecbe96270d8bf4088b
MOMOKILLER000 <buzduganmihnea@gmail.com>
2024-02-19 22:26:07 +0200

    product detail page created

--- WellnessCorner/urls.py
+++ WellnessCorner/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
-from .views import registration_view, login_view, add_allergy
+from .views import registration_view, login_view
 urlpatterns = [
     path('', views.index, name='index'),
     path('rate/<int:product_id>/<str:source>/', views.rate_product, name='rate_product'),
     path('register/', registration_view, name='register'),
     path('login/', login_view, name='login'),
-    path('add_allergy/', add_allergy, name='add_allergy'),
+    path('product/<int:product_id>/<str:source>/', views.product_detail, name='product_detail'),
 ]
--- WellnessCorner/views.py
+++ WellnessCorner/views.py
@@ -3,25 +3,30 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponseBadRequest
 from django.contrib.auth import authenticate, login
 from .models import Product, ApiProduct
-from .forms import RegistrationForm
+from .forms import RegistrationForm, AllergyForm
 import requests
 from decimal import Decimal
 import json
 from django.contrib import messages 
-from .forms import AllergyForm # Import the form for adding allergies
-from .models import User
-from django.db.models import Q
-from django.core.exceptions import ObjectDoesNotExist
-# views.py
-# views.py
+from django.core.exceptions import ValidationError
+from django.shortcuts import get_object_or_404
+
 def index(request):
     if request.method == 'POST':
         product_name = request.POST.get('product_name', '')
         product_info, common_allergies = search_product(product_name, user=request.user)
+        # Set the source attribute for each product based on its type
+        for product in product_info:
+            if isinstance(product, Product):
+                product.source = 'database'
+            elif isinstance(product, ApiProduct):
+                product.source = 'api'
+            else:
+                # Handle other cases if needed
+                pass
         return render(request, 'index.html', {'product_info': product_info, 'common_allergies': common_allergies, 'searched_product': product_name})
     else:
         return render(request, 'index.html')
-
 
 def registration_view(request):
     if request.method == 'POST':
@@ -45,47 +50,23 @@ def login_view(request):
 
         if user is not None:
             login(request, user)
-            # Redirect to the exploresphere page upon successful login
-            return redirect('index')
+            return redirect('index')  # Redirect to the index page upon successful login
         else:
             messages.error(request, 'Invalid login credentials. Please try again.')
 
     return render(request, 'login.html')
 
-def add_allergy(request):
-    if request.method == 'POST':
-        form = AllergyForm(request.POST)
-        if form.is_valid():
-            allergy = form.save()  # Save allergy instance
-            # You may want to associate this allergy with the current user
-            # For example:
-            # request.user.allergies.add(allergy)
-            return redirect('register')  # Redirect to registration page after adding allergy
-    else:
-        form = AllergyForm()
-
-    # If form is invalid, or it's a GET request, render the registration page again with the form
-    return render(request, 'register.html', {'allergy_form': form})
-
-def search_product(product_name, exclude_allergy=None, user=None):
-    # Search for products in both local database and external API
+def search_product(product_name, user=None):
     database_products = Product.objects.filter(product_name__icontains=product_name)
     api_products = []
-
-    # Retrieve user allergies if authenticated
     common_allergies = set()
+
     if user and user.is_authenticated:
         user_allergies = user.allergies.all()
 
-        # Calculate common allergies for database products
-        for product in database_products:
-            if product.allergies:
-                allergies_list = json.loads(product.allergies)  # Parse allergies JSON string
-                common_allergies.update(set(allergies_list) & set(allergy.name for allergy in user_allergies))
-
-    # Fetch products from the external API
     api_url = f'https://world.openfoodfacts.org/cgi/search.pl?search_terms={product_name}&search_simple=1&action=process&json=1'
     response = requests.get(api_url)
+
     if response.status_code == 200:
         product_data = response.json()
         if 'products' in product_data:
@@ -104,10 +85,8 @@ def search_product(product_name, exclude_allergy=None, user=None):
                     'energy-kcal_100g', '') else None
                 allergies = product.get('allergens_tags', [])
 
-                # Extract actual allergy names from the list
                 allergies = [allergy.split(':')[1] for allergy in allergies]
 
-                # Create or update API product
                 api_product, created = ApiProduct.objects.update_or_create(
                     product_name=name,
                     defaults={
@@ -118,22 +97,17 @@ def search_product(product_name, exclude_allergy=None, user=None):
                         'carbs_per_100g': carbs_per_100g,
                         'fats_per_100g': fats_per_100g,
                         'kcal_per_100g': kcal_per_100g,
-                        'allergies': json.dumps(allergies)  # Save allergies as JSON string
+                        'allergies': json.dumps(allergies) if allergies else None
                     }
                 )
                 api_products.append(api_product)
 
-                # Calculate common allergies for API products
                 if user and user.is_authenticated:
                     common_allergies.update(set(allergies) & set(allergy.name for allergy in user_allergies))
 
-    # Combine database and API products
     all_products = list(database_products) + api_products
 
     return all_products, common_allergies
-
-
-
 
 def rate_product(request, product_id, source):
     if request.method == 'POST':
@@ -155,7 +129,6 @@ def rate_product(request, product_id, source):
             if product.user_rating is None:
                 product.user_rating = rating
             else:
-                # Update the rating by averaging with previous ratings
                 product.user_rating = (product.user_rating + rating) / Decimal(2)
             product.save()
         except (Product.DoesNotExist, ApiProduct.DoesNotExist):
@@ -164,3 +137,33 @@ def rate_product(request, product_id, source):
             return HttpResponseBadRequest("Invalid rating")
 
     return redirect('index')
+
+# views.py
+def product_detail(request, product_id, source):
+    try:
+        if source == 'database':
+            product_model = Product
+        elif source == 'api':
+            product_model = ApiProduct
+        else:
+            return HttpResponseBadRequest("Invalid source")
+
+        product = get_object_or_404(product_model, pk=product_id)
+
+        # Get the current user's allergies
+        user_allergies = request.user.allergies.all()
+
+        # Parse product's allergies and find common ones with the user
+        try:
+            product_allergies = json.loads(product.allergies) if product.allergies else []
+        except json.JSONDecodeError:
+            product_allergies = []
+
+        common_allergies = set(product_allergies) & set(allergy.name for allergy in user_allergies)
+
+        # Get product rating
+        product_rating = getattr(product, 'user_rating', None)
+
+        return render(request, 'product_detail.html', {'product': product, 'common_allergies': common_allergies, 'product_rating': product_rating})
+    except product_model.DoesNotExist:
+        return HttpResponseBadRequest("Product not found")
